cb00fb11f321c1c5a21dfec34c6ab6dce5f59055
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-05-26 13:33:30 +0200

    VideoStoreTests and RentalTests not implemented

--- VideoStore/VideoStore.Tests/VideoStoreTests.cs
+++ VideoStore/VideoStore.Tests/VideoStoreTests.cs
@@ -18,7 +18,79 @@ namespace VideoStore.Tests
         [Test]
         public void MovieTitleCanNotBeEmpty()
         {
-            
+
         }
+        [Test]
+        public void AddingFourthCopyOfSameMovieNotPossible()
+        {
+
+        }
+        [Test]
+        public void ShouldNotBeAbleToAddSameCustomerTwice()
+        {
+
+        }
+        [Test]
+        public void AddingACustomerMustFollowSSNFormat()
+        {
+
+        }
+
+        [Test]
+        public void NotBeAbleToRentNonExistentMovie()
+        {
+
+        }
+        [Test]
+        public void UnregisteredCustomerCannotRentMovie()
+        {
+
+        }
+    }
+    [TestFixture]
+    public class RentalTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void BeingAbleToAddRental()
+        {
+
+        }
+        [Test]
+        public void AllRentalsGetA3DayLaterDueDate()
+        {
+
+        }
+        [Test]
+        public void ShouldBeAbleToGetRentalsBySSID()
+        {
+
+        }
+        [Test]
+        public void CanRentMoreThanOneMovie()
+        {
+
+        }
+        [Test]
+        public void CannotRentMoreThan3Movies()
+        {
+
+        }
+        [Test]
+        public void CustomersMayNotPossessTwoCopiesOfTheSameMovie()
+        {
+
+        }
+        [Test]
+        public void CustomersMayNotRentAnymoreMoviesIfTheyHaveLateDueDateMovies()
+        {
+            //Assert.Throws<Exceptionx>(() => sut.DoStuff());
+        }
+
+        // more tests here
     }
 }
